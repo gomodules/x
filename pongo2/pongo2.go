46bccd4e91ea3dd99eaf703e3a3f473510576626
Tamal Saha <tamal@appscode.com>
2016-12-22 11:52:11 -0800

    Add pongo2 pkg

---
+++ pongo2/pongo2.go
@@ -0,0 +1,37 @@
+package pongo2
+
+import (
+	"encoding/json"
+
+	p "github.com/flosch/pongo2"
+)
+
+func YAMLSafeContext(v interface{}) (*p.Context, error) {
+	data, err := json.Marshal(v)
+	if err != nil {
+		return nil, err
+	}
+
+	before := make(map[string]interface{})
+	err = json.Unmarshal(data, &before)
+	if err != nil {
+		return nil, err
+	}
+
+	after := make(map[string]interface{})
+	for k, v := range before {
+		switch u := v.(type) {
+		case bool:
+			if u {
+				after[k] = "true"
+			} else {
+				after[k] = "false"
+			}
+		default:
+			after[k] = v
+		}
+	}
+
+	ptx := p.Context(after)
+	return &ptx, nil
+}
