fc02fadf4a910e70c40cd03ea68777ecb50bf390
tamal <tamal@appscode.com>
2017-03-27 13:22:46 -0700

    Support writing http response body bytes into writer.

--- net/httpclient/httpclient.go
+++ net/httpclient/httpclient.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -228,9 +229,16 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 	}
 
 	if v != nil {
-		err := json.NewDecoder(resp.Body).Decode(v)
-		if err != nil {
-			return nil, err
+		if w, ok := v.(io.Writer); ok {
+			_, err := io.Copy(w, resp.Body)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			err := json.NewDecoder(resp.Body).Decode(v)
+			if err != nil {
+				return nil, err
+			}
 		}
 	}
 	return resp, err
