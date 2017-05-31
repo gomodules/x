4c82fbd77acc0cc1cf693aaf40832102896bad4a
Tamal Saha <tamal@appscode.com>
2017-05-31 14:55:52 -0700

    Add new type StrYo that always turns input into a string (#9)

---
+++ encoding/json/types/str_yo_test.go
@@ -0,0 +1,41 @@
+package types_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"fmt"
+
+	. "github.com/appscode/go/encoding/json/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStrYo(t *testing.T) {
+	assert := assert.New(t)
+
+	type Example struct {
+		A StrYo
+		B StrYo
+		C StrYo
+		D StrYo
+		E StrYo
+	}
+	s := `{
+		"A": "str\\",
+		"B": 1,
+		"C": 2.5,
+		"D": false,
+		"E": true
+	}`
+
+	var e Example
+	err := json.Unmarshal([]byte(s), &e)
+
+	assert.Nil(err)
+	b, err := json.Marshal(&e)
+
+	fmt.Println(e.A)
+
+	fmt.Println(string(b))
+	assert.Equal(`{"A":"str\\","B":"1","C":"2.5","D":"false","E":"true"}`, string(b))
+}
