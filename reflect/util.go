86ed1133ef8d7ff33d8ec5b68903f1bd1b7920a9
tamal <tamal@appscode.com>
2017-09-23 15:10:34 -0700

    Add IsZero()

---
+++ reflect/util.go
@@ -0,0 +1,26 @@
+package reflect
+
+import r "reflect"
+
+// https://stackoverflow.com/a/23555352/244009
+func IsZero(v r.Value) bool {
+	switch v.Kind() {
+	case r.Func, r.Map, r.Slice:
+		return v.IsNil()
+	case r.Array:
+		z := true
+		for i := 0; i < v.Len(); i++ {
+			z = z && IsZero(v.Index(i))
+		}
+		return z
+	case r.Struct:
+		z := true
+		for i := 0; i < v.NumField(); i++ {
+			z = z && IsZero(v.Field(i))
+		}
+		return z
+	}
+	// Compare other types directly:
+	z := r.Zero(v.Type())
+	return v.Interface() == z.Interface()
+}
