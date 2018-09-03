47529af3f3e354c1488d4240e3f8f2f82ef96780
Tamal Saha <tamal@appscode.com>
2018-09-03 07:31:55 -0700

    Add IntHash type (#34)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ encoding/json/types/int_hash_test.go
@@ -0,0 +1,44 @@
+package types_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	. "github.com/appscode/go/encoding/json/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntHash_Empty(t *testing.T) {
+	assert := assert.New(t)
+
+	var x IntHash
+	err := x.UnmarshalJSON([]byte(`"5$ty"`))
+	assert.Nil(err)
+}
+
+func TestIntHash(t *testing.T) {
+	assert := assert.New(t)
+
+	type Example struct {
+		A IntHash
+		B IntHash
+		C IntHash
+		D IntHash
+		E *IntHash
+		F *IntHash `json:",omitempty"`
+	}
+	s := `{
+		"A": "0$str\\",
+		"B": 1,
+		"C": "8$xyz",
+		"E": null
+	}`
+
+	var e Example
+	err := json.Unmarshal([]byte(s), &e)
+	assert.Nil(err)
+
+	b, err := json.Marshal(&e)
+	assert.Nil(err)
+	assert.Equal(`{"A":"0$str\\","B":1,"C":"8$xyz","D":0,"E":null}`, string(b))
+}
