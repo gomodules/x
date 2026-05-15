59cfd309355f4b8a937b0b0ec63ffa9a31b2acb5
Tamal Saha <tamal@appscode.com>
2026-05-15 13:31:36 +0600

    Harden CI workflows
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- net/httpclient/doc.go
+++ net/httpclient/doc.go
@@ -2,7 +2,7 @@
 Package httpclient provides a simple Golang Client for JSON Http Microservices
 
 Acknowledgement:
- - [DigitalOcean Go client](https://github.com/digitalocean/godo/blob/master/godo.go)
- - [OVH Go client](https://github.com/ovh/go-ovh/blob/master/ovh/ovh.go)
+  - [DigitalOcean Go client](https://github.com/digitalocean/godo/blob/master/godo.go)
+  - [OVH Go client](https://github.com/ovh/go-ovh/blob/master/ovh/ovh.go)
 */
 package httpclient
